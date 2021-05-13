ac4c5597efeea9e3720b16888d767038b5ad5e21
Bernard <bernard.hanna@webinnovate.ie>
2021-05-13 10:58:47 +0100

    intial comit

---
+++ css/main.cs
@@ -0,0 +1,15 @@
+html {
+  width: 100%;
+  max-width: 1920px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+html {
+  width: 100%;
+  max-width: 1920px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+/*# sourceMappingURL=main.cs.map */
