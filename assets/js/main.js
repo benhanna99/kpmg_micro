ffd372231315a45527888d74051581b04a90e57a
Bernard <bernard.hanna@webinnovate.ie>
2021-05-14 08:58:29 +0100

    message

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+ //Sticky Header 
+  // Set options
+        var options = {
+            offset: '#showHere',
+            offsetSide: 'top',
+            classes: {
+                clone:   'banner--clone',
+                stick:   'banner--stick',
+                unstick: 'banner--unstick'
+            }
+        };
+
+        // Initialise with options
+        var banner = new Headhesive('.banner', options);
+
+        // Headhesive destroy
+        // banner.destroy();
